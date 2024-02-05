42438f5591512bc69970e04ba633634cdab5c5c1
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2024-02-05 21:54:57 +0100

    update

--- script.js
+++ script.js
@@ -1,48 +1,39 @@
-// const baseUrl = 'https://api.pexels.com/v1/search?query=';
-// const apiKey = 'hlGi7oSsJeXRzMIlZadnCzDCEIZeb9KtuzwDczHtH6Msx6c4za18jFni';
+const baseUrl = 'https://api.pexels.com/v1/search?query=';
+const apiKey = 'hlGi7oSsJeXRzMIlZadnCzDCEIZeb9KtuzwDczHtH6Msx6c4za18jFni';
 
-// const commonHeaders = {
-//     'Content-Type': 'application/json; charset=utf-8',
-//     'Authorization': apiKey,
-// };
+const fetchData = async (endpoint) => {
+    const url = `${baseUrl}${endpoint}`;
+    const requestOptions = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json; charset=utf-8',
+            'Authorization': apiKey,
+        },
+    };
 
-// function fetchData(endpoint) {
-//     const url = `${baseUrl}/${endpoint}`;
-//     const requestOptions = {
-//         method: 'GET',
-//         headers: commonHeaders,
-//         body: null,
-//     };
+    try {
+        const response = await fetch(url, requestOptions);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return await response.json();
+    } catch (error) {
+        console.error('Error detected:', error);
+    }
+};
 
-//     return fetch(url, requestOptions)
-//         .then(response => {
-//             if (!response.ok) {
-//                 throw new Error(`HTTP error! Status: ${response.status}`);
-//             }
-//             return response.json();
-//         })
-//         .catch(error => {
-//             console.error('Error detected:', error);
-//         });
-// }
+const buttonSearch = document.getElementById('buttonSearch');
+const searchInput = document.getElementById('searchInput');
 
-// fetchData('ocean')
-//     .then(json => console.log(json));
-
-let buttonSearch = document.getElementById('buttonSearch');
-
-let searchInput = document.getElementById('searchInput');
-
-buttonSearch.addEventListener('click', () => {
-    let value = searchInput.value; // Rimuovi gli spazi vuoti iniziali e finali
+buttonSearch.addEventListener('click', async () => {
+    const value = searchInput.value.trim(); // Rimuovi gli spazi vuoti iniziali e finali
     console.log(value);
 
-    fetch(`https://api.pexels.com/v1/search?query=${query}`)
-        .then((response) => response.json())
-        .then((json) => {
-            data = json;
-            console.log(data);
-
-        })
-        .catch((err) => console.error("Error detected: ", err));
+    try {
+        const data = await fetchData(value);
+        console.log(data);
+    } catch (error) {
+        console.error('Error detected:', error);
+    }
 });
+
