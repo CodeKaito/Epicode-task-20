aca97258bcf6dffb4633c5dd78f42e316e287cd5
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2024-02-05 21:36:43 +0100

    update

--- script.js
+++ script.js
@@ -1,32 +1,48 @@
-const baseUrl = 'https://api.pexels.com/v1/search?query=';
-const apiKey = 'hlGi7oSsJeXRzMIlZadnCzDCEIZeb9KtuzwDczHtH6Msx6c4za18jFni';
+// const baseUrl = 'https://api.pexels.com/v1/search?query=';
+// const apiKey = 'hlGi7oSsJeXRzMIlZadnCzDCEIZeb9KtuzwDczHtH6Msx6c4za18jFni';
 
-const commonHeaders = {
-    'Content-Type': 'application/json; charset=utf-8',
-    'Authorization': apiKey,
-};
+// const commonHeaders = {
+//     'Content-Type': 'application/json; charset=utf-8',
+//     'Authorization': apiKey,
+// };
 
-function fetchData(endpoint) {
-    const url = `${baseUrl}/${endpoint}`;
-    const requestOptions = {
-        method: 'GET',
-        headers: commonHeaders,
-        body: null,
-    };
+// function fetchData(endpoint) {
+//     const url = `${baseUrl}/${endpoint}`;
+//     const requestOptions = {
+//         method: 'GET',
+//         headers: commonHeaders,
+//         body: null,
+//     };
 
-    return fetch(url, requestOptions)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-            return response.json();
+//     return fetch(url, requestOptions)
+//         .then(response => {
+//             if (!response.ok) {
+//                 throw new Error(`HTTP error! Status: ${response.status}`);
+//             }
+//             return response.json();
+//         })
+//         .catch(error => {
+//             console.error('Error detected:', error);
+//         });
+// }
+
+// fetchData('ocean')
+//     .then(json => console.log(json));
+
+let buttonSearch = document.getElementById('buttonSearch');
+
+let searchInput = document.getElementById('searchInput');
+
+buttonSearch.addEventListener('click', () => {
+    let value = searchInput.value; // Rimuovi gli spazi vuoti iniziali e finali
+    console.log(value);
+
+    fetch(`https://api.pexels.com/v1/search?query=${query}`)
+        .then((response) => response.json())
+        .then((json) => {
+            data = json;
+            console.log(data);
+
         })
-        .catch(error => {
-            console.error('Error detected:', error);
-        });
-}
-
-fetchData('ocean')
-    .then(json => console.log(json));
-
-
+        .catch((err) => console.error("Error detected: ", err));
+});
